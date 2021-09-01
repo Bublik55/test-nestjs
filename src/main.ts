039622f718f7a6e0705b5e011c15e86c3369d9a6
Bublik55 <user211195@gmail.com>
2021-09-01 15:37:47 +0600

    empty

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,21 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);	
+  const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   const PORT = process.env.PORT;
   const config = new DocumentBuilder()
     .setTitle('Example')
-    .setDescription('Auth/SignUp is public route. All requests to other routes must contain jwt token.')
+    .setDescription(
+      `Auth/SignUp is public route. All requests to other routes must contain jwt token.
+      User can create column by <b>users/{userid}/colummns</b> route in one case:\n
+      \t Client's userID == {userID}\n
+      User can update and delete sources in one case - he/she is author of source.\n
+      Data from DB will not serialized.\n 
+      `,
+    )
     .setVersion('0.0.1')
-	.addBearerAuth()
+    .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
--- src/utils/auth/auth.controller.ts
+++ src/utils/auth/auth.controller.ts
@@ -1,37 +1,35 @@
-import { Controller, Body, Post, UseGuards, Request, Req } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from '../../dtos';
-import { ApiOperation, ApiOkResponse, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { Users } from 'src/models/users.model';
+import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
-import { LocalStrategy } from './strategies/local.strategy';
 import { Public } from './guards/jwt-auth.guard';
+import { LocalStrategy } from './strategies/local.strategy';
 
 @Public()
 @ApiTags(`Auth`)
 @Controller('auth')
 export class AuthController {
-    constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService) {}
 
-	@Post('login')
-	@UseGuards(LocalStrategy)
-	@ApiOperation({summary: 'Login User'})
-	@ApiOkResponse({
-		status: 201,
-		description: 'Ok login',
-	})
-	async login(@Body() loginDto: LoginDto,) {
-        return await this.authService.login(loginDto);
-    }
+  @Post('login')
+  @UseGuards(LocalStrategy)
+  @ApiOperation({ summary: 'Login User' })
+  @ApiOkResponse({
+    status: 200,
+    description: 'Ok login',
+  })
+  async login(@Body() loginDto: LoginDto) {
+    return await this.authService.login(loginDto);
+  }
 
-
-	@Post(`signup`)
-	@ApiOperation({summary: 'SignUp User'})
-	@ApiOkResponse({
-		status: 201,
-		description: 'User registred',
-	})
-    async signUp(@Body() createUserDto: CreateUserDto) {
-        return await this.authService.create(createUserDto);
-    }
+  @Post(`signup`)
+  @ApiOperation({ summary: 'SignUp User' })
+  @ApiOkResponse({
+    status: 201,
+    description: 'User registred',
+  })
+  async signUp(@Body() createUserDto: CreateUserDto) {
+    return await this.authService.create(createUserDto);
+  }
 }
